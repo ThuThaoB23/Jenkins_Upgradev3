a0b038ebcfdc46012ff3a361e3a8345920fe03bc
Tran Thu Thao <ThaoTT.B23MR314@stu.ptit.edu.vn>
2025-06-24 16:21:36 +0700

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -28,12 +28,12 @@ public class TestGreeter {
   }
   
 
-  // @Test
-  // public void greetShouldIncludeGreetingPhrase() {
-  //   String someone = "World";
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World";
 
-  //   assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  // }
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
